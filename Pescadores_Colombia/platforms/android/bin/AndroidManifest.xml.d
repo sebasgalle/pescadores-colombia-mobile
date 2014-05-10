ba9511b6654c1d5569e4c5796881e3a957d6ce95
Sebastian Gallego Herrera <segaher13@gmail.com>
2014-05-10 14:54:44 -0500

    sebagahe - Restructuracion de carpetas

--- Pescadores_Colombia/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-B:\Repositorios\GitHub\pescadores-colombia-mobile\Pescadores_Colombia\platforms\android\bin\AndroidManifest.xml : \
-B:\Repositorios\GitHub\pescadores-colombia-mobile\Pescadores_Colombia\platforms\android\AndroidManifest.xml \
--- Pescadores_Colombia/platforms/android/facebook/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-B:\Repositorios\GitHub\pescadores-colombia-mobile\Pescadores_Colombia\platforms\android\facebook\bin\AndroidManifest.xml : \
-B:\Repositorios\GitHub\pescadores-colombia-mobile\Pescadores_Colombia\platforms\android\facebook\AndroidManifest.xml \
