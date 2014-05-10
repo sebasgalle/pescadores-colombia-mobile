7b5a8d2622c888613345cedc44ae1b0df45f7023
Sebastian Gallego Herrera <segaher13@gmail.com>
2014-05-10 14:53:57 -0500

    sebagahe - Restructuracion de carpetas

--- Pescadores_Colombia/www/js/aplicacion/index.js
+++
@@ -1,9 +0,0 @@
-function iniciarAplicacion(ruta){
-	document.addEventListener("deviceready", redireccionar(ruta), true);
-}
-
-function redireccionar(ruta){
-	$( document ).ready(function() {
-		location.href = ruta;
-	});
-}
