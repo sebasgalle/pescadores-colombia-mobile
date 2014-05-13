b4bd9396e4c19aa20f8ce15c5d35f9d3f660cda5
jadiosa <jadiosa@gmail.com>
2014-05-13 17:57:37 -0500

    se incluye soporte para plataforma ios

---
+++ platforms/ios/www/js/utilities/facebook_utilities.js
@@ -0,0 +1,14 @@
+/*
+---------------------------------------------------------------------------------------------------
+									JAVASCRIPT DOCUMENTATION
+---------------------------------------------------------------------------------------------------
+Description:
+This file contain functions that allow the application to access facebook.
+
+Author:
+Spinner Lab's Development Team.
+*/
+
+// Local Variables
+
+// Functions
